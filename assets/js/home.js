462211b7f682da048e08884c34adbc50516323f2
Emin <eminjk@code.edu.az>
2022-02-16 13:48:08 -0800

    footter and 2 section completed

--- assets/js/home.js
+++ assets/js/home.js
@@ -26,5 +26,23 @@ clock.start();
 // set time
 
 
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+
 
 
